f98c6d336c0f747632427b9a5babd09504764809
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-13 19:40:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,23 @@
-import os
-from transformers import AutoTokenizer, pipeline, logging
-from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
-import argparse
-
-
-os.environ["TOKENIZERS_PARALLELISM"] = "true"
+from transformers import AutoTokenizer
+from auto_gptq import AutoGPTQForCausalLM
 
 class InferlessPythonModel:
-    def initialize(self):
-        model_id = "TheBloke/vicuna-7B-v1.3-GPTQ"
-        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
-        self.model = AutoGPTQForCausalLM.from_quantized(model_id,
-            use_safetensors=True,
-            trust_remote_code=True,
-            device_map='auto',
-            use_triton=True,
-            quantize_config=None
-        )
+  def initialize(self):
+      self.tokenizer = AutoTokenizer.from_pretrained("TheBloke/vicuna-7B-v1.3-GPTQ", use_fast=True)
+      self.model = AutoGPTQForCausalLM.from_quantized(
+        "TheBloke/vicuna-7B-v1.3-GPTQ",
+        use_safetensors=True,
+        device="cuda:0",
+        quantize_config=None,
+        inject_fused_attention=False
+      )
 
+  def infer(self, inputs):
+    prompt = inputs["prompt"]
+    input_ids = self.tokenizer(prompt, return_tensors='pt').input_ids.cuda()
+    output = self.model.generate(inputs=input_ids, temperature=0.7, max_new_tokens=200)
+    result = self.tokenizer.decode(output[0])
+    return {"generated_result": result}
 
-    def infer(self, inputs):
-        prompt = inputs["prompt"]
-        prompt_template=f'''USER: {prompt}
-        ASSISTANT:'''
-
-        pipe = pipeline(
-            "text-generation",
-            model=self.model,
-            tokenizer=self.tokenizer,
-            max_new_tokens=512,
-            temperature=0.7,
-            top_p=0.95,
-            repetition_penalty=1.15
-        )
-        generated_text = pipe(prompt_template)[0]['generated_text']
-        return {"generated_text": generated_text}
-
-    def finalize(self):
-        self.model = None
+  def finalize(self,args):
+    self.model = None
